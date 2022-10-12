221cd4c1635e7cc27a826300303e4bca417b39ee
AlexKent64 <alexander.kent@ashbury.ca>
2022-10-12 17:36:57 -0400

    yuh

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 
 
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
 public class Main {
 
 
     public static void main(String[] args) {
 
+        final String url = "https://www.pc-canada.com/"; //the website url that is being scraped
 
+
+        try {
+            final Document document = Jsoup.connect(url).get(); //Using the Jsoup class, connect to the page that the url is connected to.
+
+            System.out.println(document.outerHtml()); //Output all HTML of the webpage from the url.
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
 
     }
 
